5e51ee0a51388b66e0f72972f4e33616c0aeb302
wanumair09 <wanumair09@gmail.com>
2026-01-15 10:27:03 +0800

    update student safety

--- app/src/main/java/com/example/smartcampusapp/EmergencyActivity.java
+++ app/src/main/java/com/example/smartcampusapp/EmergencyActivity.java
@@ -34,6 +34,14 @@ public class EmergencyActivity extends AppCompatActivity {
         dbHelper = new SafetyDatabaseHelper(this);
 
         btnEmergency.setOnClickListener(v -> sendEmergencyAlert());
+        if (checkSelfPermission(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
     }
 
     private void sendEmergencyAlert() {
@@ -72,17 +80,16 @@ public class EmergencyActivity extends AppCompatActivity {
 
     // Handle permission result
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == LOCATION_PERMISSION_CODE &&
-                grantResults.length > 0 &&
-                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            sendEmergencyAlert();
+        if (requestCode == 100) {
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Location permission granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Location permission denied", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
